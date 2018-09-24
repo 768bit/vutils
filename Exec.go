ac551107b65c4a196296db543f9e282c9ec9f584
Craig Smith <craig.smith@768bit.com>
2018-09-24 16:18:42 +0100

    updates

--- Exec.go
+++ Exec.go
@@ -3,11 +3,13 @@ package vutils
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
@@ -15,29 +17,75 @@ type execUtils struct {
 }
 
 type ExecAsyncCommand struct {
-	Proc    *exec.Cmd
-	errOnly bool
-	reader  io.ReadCloser
-	error   io.ReadCloser
-	writer  io.WriteCloser
+	Proc     *exec.Cmd
+	errOnly  bool
+	reader   io.ReadCloser
+	error    io.ReadCloser
+	writer   io.WriteCloser
+	intChan  chan os.Signal
+	intBound bool
 }
 
-func (ec *ExecAsyncCommand) BindToStdoutAndStdErr() {
+func (ec *ExecAsyncCommand) BindToStdoutAndStdErr() *ExecAsyncCommand {
 	if !ec.errOnly {
-		scanner := bufio.NewScanner(ec.reader)
 		go func() {
-			for scanner.Scan() {
-				log.Print(scanner.Text())
-			}
+			io.Copy(os.Stdout, ec.reader)
 		}()
 	}
 
-	errScanner := bufio.NewScanner(ec.error)
 	go func() {
-		for errScanner.Scan() {
-			log.Print(errScanner.Text())
-		}
+		io.Copy(os.Stderr, ec.error)
 	}()
+	return ec
+}
+
+func (ec *ExecAsyncCommand) SetWorkingDir(path string) *ExecAsyncCommand {
+	ec.Proc.Dir = path
+	return ec
+}
+
+func (ec *ExecAsyncCommand) CopyEnv() *ExecAsyncCommand {
+	env := os.Environ()
+	return ec.SetEnv(env)
+}
+
+func (ec *ExecAsyncCommand) AddEnv(key string, value string) *ExecAsyncCommand {
+	ec.Proc.Env = append(ec.Proc.Env, fmt.Sprintf(`%s=%s`, key, value))
+	return ec
+}
+
+func (ec *ExecAsyncCommand) SetEnv(env []string) *ExecAsyncCommand {
+	ec.Proc.Env = env
+	return ec
+}
+
+func (ec *ExecAsyncCommand) StartAndWait() error {
+	if !ec.errOnly {
+		defer ec.reader.Close()
+	}
+	if ec.intChan != nil && ec.intBound {
+		defer close(ec.intChan)
+	}
+	defer ec.writer.Close()
+	defer ec.error.Close()
+	fmt.Printf("$: %s %s\n", ec.Proc.Path, strings.Join(ec.Proc.Args, ` `))
+	if err := ec.Proc.Start(); err != nil {
+		return err
+	} else if err := ec.Proc.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ec *ExecAsyncCommand) BindSigIntHandler() *ExecAsyncCommand {
+	ec.intBound = true
+	ec.intChan = make(chan os.Signal)
+	signal.Notify(ec.intChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ec.intChan
+		ec.Proc.Process.Signal(syscall.SIGTERM)
+	}()
+	return ec
 }
 
 func (ex *execUtils) CreateExecCommand(path string, args ...string) *exec.Cmd {
@@ -160,8 +208,9 @@ func (ex *execUtils) RunCommandAsyncOutput(path string, errOnly bool, args ...st
 func (ex *execUtils) CreateAsyncCommand(path string, errOnly bool, args ...string) *ExecAsyncCommand {
 
 	pr := ExecAsyncCommand{
-		Proc:    exec.Command(path, args...),
-		errOnly: errOnly,
+		Proc:     exec.Command(path, args...),
+		errOnly:  errOnly,
+		intBound: false,
 	}
 
 	if !errOnly {
