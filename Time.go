405ce1bc47bf66c41a32a097ef9c490ef9f98545
Craig Smith <craig.smith@768bit.com>
2018-12-21 04:54:28 +0000

    Adding DST offsest calcss

--- Time.go
+++ Time.go
@@ -23,4 +23,23 @@ func (tu *timeUtils) IsDaylightSavingsTime(inTime time.Time) bool {
 	return false
 }
 
+func (tu *timeUtils) GetDaylightSavingsTimeOffset(inTime time.Time) time.Duration {
+	_, timeOffset := inTime.Zone() //with time already in correct myLocation
+	winterTime := time.Date(inTime.Year(), 1, 1, 0, 0, 0, 0, inTime.Location())
+	summerTime := time.Date(inTime.Year(), 7, 1, 0, 0, 0, 0, inTime.Location())
+	_, winterOffset := winterTime.Zone()
+	_, summerOffset := summerTime.Zone()
+
+	if winterOffset > summerOffset {
+		winterOffset, summerOffset = summerOffset, winterOffset
+	}
+
+	if winterOffset != summerOffset { // the location has daylight saving
+		if timeOffset != winterOffset {
+			return summerTime.Sub(winterTime)
+		}
+	}
+	return time.Duration(0)
+}
+
 var Time = &timeUtils{}
